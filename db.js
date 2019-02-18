d67a27d86d378ab9e2c3d0b13903e89c20f14f0b
Kevin Welch <kaedub16@gmail.com>
2019-02-17 21:55:16 -0800

    fix in config and db files to parse database name for Pool config object

--- db.js
+++ db.js
@@ -2,19 +2,23 @@
 
 const Pool = require('pg-pool');
 const url = require('url');
-const { DB_URI } = require("./config");
+const { DB_URI } = require('./config');
 
 const params = url.parse(DB_URI);
-const auth = params.auth.split(':');
+const [user, password] = params.auth.split(':');
+const database = params.pathname.slice(1);
+const host = params.hostname;
 
 const config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
+  user,
+  password,
+  host,
+  database,
   port: 5432,
-  database: 'libellis',
   ssl: false
 };
+
+console.log(config);
 
 const pool = new Pool(config);
 
